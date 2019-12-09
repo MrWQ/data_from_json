2cfa7369d3b9802c1638bf6214142e1638ab2f7c
1061563931@qq.com <Wq431879>
2019-12-09 17:22:27 +0800

    修复部分bug，添加滚动条

--- data_from_json.py
+++ data_from_json.py
@@ -15,36 +15,45 @@ def get_data():
     input_json = input_text.get("0.0", "end")
     try:
         decode_json = demjson.decode(input_json)
+        print(decode_json, type(decode_json))
     except:
         output_text.delete('0.0', 'end')
-        output_text.insert('end', ' json解析错误 ')
+        output_text.insert('end', ' json解析错误,检查输入是否为json ')
         return
     # 获取数据提取路径
     data_path = get_path_entry.get()
-    temp = 'json' + str(data_path)
-    repl = "output_data = " + temp
-    print(repl)
+    temp_1 = 'json' + str(data_path)
+    repl_1 = "output_data = " + temp_1
+    print(repl_1)
+    temp_2 = 'decode_json' + str(data_path)
+    repl_2 = "output_data = " + temp_2
+    print(repl_2)
     exec_to_get = """
-if type(decode_json) == type([]):
+if type(decode_json) == type([]):   
+    # 如果为列表中字典
     output_text.delete('0.0', 'end')
     for json in decode_json:
         try:
-            here_will_be_replaced
+            here_will_be_replaced_1
+            print(output_data, type(output_data))
         except:
-            output_data = ' 路径解析错误 '
+            output_data = ' 路径解析错误（类型：列表中json） '
         finally:
             output_data = str(output_data) + '\\n'
             output_text.insert('end', str(output_data))
 else:
+    # 如果为字典
     output_text.delete('0.0', 'end')
     try:
-        here_will_be_replaced
+        here_will_be_replaced_2
+        print(output_data, type(output_data))
     except:
-        output_data = ' 路径解析错误 '
+        output_data = ' 路径解析错误 (类型：json)'
     finally:
         output_text.insert('end', str(output_data))
 """
-    exec_to_get = exec_to_get.replace('here_will_be_replaced', repl)    # 通过替换来变成正确代码
+    exec_to_get = exec_to_get.replace('here_will_be_replaced_1', repl_1)    # 通过替换来变成正确代码
+    exec_to_get = exec_to_get.replace('here_will_be_replaced_2', repl_2)    # 通过替换来变成正确代码
     # print(exec_to_get)
     exec(exec_to_get)       # 将字符串作为代码执行
 
@@ -57,7 +66,7 @@ window.title("data_from_json     by:MrWQ     https://github.com/MrWQ")
 # input_name = tk.Label(window, text='json')
 input_text = tk.Text(window, width=80, height=15)
 output_text = tk.Text(window, width=80, height=15)
-change_frame = tk.LabelFrame(window, height=200, width=300, text='选择游戏')
+change_frame = tk.LabelFrame(window, height=200, width=300)
 get_path_entry = tk.Entry(change_frame, width=50)
 get_data_button = tk.Button(change_frame, text='get data', width=10, height=1, compound='left', command=get_data)
 reset_button = tk.Button(change_frame, text='clear all', width=10, height=1, compound='left', command=clear)
@@ -75,5 +84,12 @@ output_text.pack(padx=10, pady=10, fill='both', expand=True)
 get_path_entry.grid(padx=10, pady=10, row=0, column=0, sticky=tk.E+tk.W)
 get_data_button.grid(padx=10, pady=10, row=0, column=1, sticky=tk.E+tk.W)
 reset_button.grid(padx=10, pady=10, row=0, column=2, sticky=tk.E+tk.W)
+
+# 设置滚动条
+input_scrollbar_y = tk.Scrollbar(input_text)  # 竖直滚动条
+input_scrollbar_y.pack(side='right', fill='y')
+output_scrollbar = tk.Scrollbar(output_text)  # 竖直滚动条
+output_scrollbar.pack(side='right', fill='y')
+
 
 window.mainloop()
